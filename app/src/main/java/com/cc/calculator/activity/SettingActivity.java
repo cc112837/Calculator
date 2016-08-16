ab9957ea0928ad6336d7d9cb0544cc1de1aa94f7
cc <cc112837@163.com>
2016-08-16 16:13:15 +0800

    添加设置界面

--- app/src/main/java/com/cc/calculator/activity/SettingActivity.java
+++ app/src/main/java/com/cc/calculator/activity/SettingActivity.java
@@ -1,14 +1,21 @@
 package com.cc.calculator.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cc.calculator.R;
 
 public class SettingActivity extends Activity {
     private ImageView back;
+    private TextView tv_volume;
+    private LinearLayout ll_msg,ll_updata,ll_clear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +25,32 @@ public class SettingActivity extends Activity {
 
     private void init() {
         back=(ImageView) findViewById(R.id.back);
+        tv_volume=(TextView) findViewById(R.id.tv_volume);
+        ll_clear=(LinearLayout) findViewById(R.id.ll_clear);
+        ll_updata=(LinearLayout) findViewById(R.id.ll_updata);
+        ll_msg=(LinearLayout) findViewById(R.id.ll_msg);
+        ll_msg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: 2016/8/16 新消息通知
+            }
+        });
+        ll_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SettingActivity.this,"清理缓存中",Toast.LENGTH_LONG).show();
+                SystemClock.sleep(2000);
+                tv_volume.setText("0M");
+                Toast.makeText(SettingActivity.this,"清理完成",Toast.LENGTH_LONG).show();
+            }
+        });
+        ll_updata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SettingActivity.this,UpdataPwdActivity.class);
+                startActivity(intent);
+            }
+        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
