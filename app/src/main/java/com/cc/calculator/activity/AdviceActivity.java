fb59c72898a3c1a89c3e64e03ae8b090669f03d2
cc <cc112837@163.com>
2016-08-16 16:42:52 +0800

    添加用户反馈页面

--- app/src/main/java/com/cc/calculator/activity/AdviceActivity.java
+++ app/src/main/java/com/cc/calculator/activity/AdviceActivity.java
@@ -3,12 +3,16 @@ package com.cc.calculator.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.cc.calculator.R;
 
 public class AdviceActivity extends Activity {
     private ImageView back;
+    private Button btn_submit;
+    private EditText et_advice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class AdviceActivity extends Activity {
 
     private void init() {
         back=(ImageView) findViewById(R.id.back);
+        btn_submit=(Button) findViewById(R.id.btn_submit);
+        et_advice=(EditText) findViewById(R.id.et_advice);
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {// TODO: 2016/8/16 提交用户意见
+                String advice=et_advice.getText().toString();
+            }
+        });
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
